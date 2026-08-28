36fe2d20321a270aea1e6ad5d8fa166d6c19481f
ilhamrf540-ship-it <ilhamrf540-ship-it@example.com>
2026-08-28 12:57:52 +0700

    Update portal absensi dan ujian militer

--- push.bat
+++ push.bat
@@ -1,12 +1,13 @@
 @echo off
-echo Menginisialisasi Git...
-git init
-git remote remove origin 2>nul
-git remote add origin https://github.com/ilhamrf540-ship-it/Staf-Renada.git
-git add .
-git commit -m "Update portal absensi dan ujian militer"
+echo Menginisialisasi Git menggunakan Portable Git...
+set GIT_PATH="C:\Users\FAIZAL RIDHO ILHAMI\.gemini\antigravity-ide\brain\e94c9e3e-4bf9-4ed7-a707-ee438f344fc8\scratch\git\cmd\git.exe"
+%GIT_PATH% init
+%GIT_PATH% remote remove origin 2>nul
+%GIT_PATH% remote add origin https://github.com/ilhamrf540-ship-it/Staf-Renada.git
+%GIT_PATH% add .
+%GIT_PATH% commit -m "Update portal absensi dan ujian militer"
 echo Melakukan Push ke Github...
-git branch -M main
-git push -u origin main
+%GIT_PATH% branch -M main
+%GIT_PATH% push -u origin main
 echo Selesai!
 pause
